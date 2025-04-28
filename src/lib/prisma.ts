a52206401aa16506a4bcdb9901b25644e5db0ab9
Dhruv Jaiswal <dhruvujjain@gmail.com>
2025-04-28 16:26:26 +0530

    first commit

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,28 +1,13 @@
+// lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-// Extend the global object type to include prisma
 declare global {
   // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-class PrismaSingleton {
-  private static instance: PrismaClient;
+const prisma = global.prisma || new PrismaClient();
 
-  private constructor() {}
-
-  public static getInstance(): PrismaClient {
-    if (!PrismaSingleton.instance) {
-      PrismaSingleton.instance = new PrismaClient();
-    }
-    return PrismaSingleton.instance;
-  }
-}
-
-// Get the PrismaClient instance
-const prisma = PrismaSingleton.getInstance();
-
-// Only set the global instance in development mode to prevent multiple instances in production
 if (process.env.NODE_ENV !== 'production') {
   global.prisma = prisma;
 }
