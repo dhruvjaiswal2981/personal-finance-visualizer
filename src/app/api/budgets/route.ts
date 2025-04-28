25ae57bfb99ead47fbc2a357f9ce6f00b61554a7
Dhruv Jaiswal <dhruvujjain@gmail.com>
2025-04-28 16:48:30 +0530

    first commit

--- src/app/api/budgets/route.ts
+++ src/app/api/budgets/route.ts
@@ -2,27 +2,37 @@ import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET() {
-  const budgets = await prisma.budget.findMany();
-  return NextResponse.json(budgets);
+  try {
+    const budgets = await prisma.budget.findMany();
+    return NextResponse.json(budgets);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Error fetching budgets' }, { status: 500 });
+  }
 }
 
 export async function POST(req: NextRequest) {
-  const data = await req.json();
+  try {
+    const data = await req.json();
 
-  const budget = await prisma.budget.upsert({
-    where: {
-      category_month: {
+    const budget = await prisma.budget.upsert({
+      where: {
+        category_month: {
+          category: data.category,
+          month: new Date(data.month),  // Ensure correct Date format
+        }
+      },
+      update: { amount: data.amount },
+      create: {
         category: data.category,
         month: new Date(data.month),  // Ensure correct Date format
+        amount: data.amount,
       }
-    },
-    update: { amount: data.amount },
-    create: {
-      category: data.category,
-      month: new Date(data.month),  // Ensure correct Date format
-      amount: data.amount,
-    }
-  });
+    });
 
-  return NextResponse.json(budget);
+    return NextResponse.json(budget);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Error creating/updating budget' }, { status: 500 });
+  }
 }
